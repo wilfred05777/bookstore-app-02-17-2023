44b702eeede5b4af40905dc2738954693607101d
Wilfred <wilfred05777@gmail.com>
2023-02-17 20:08:24 +0800

    install packages in server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,11 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 
 const app = express()
 const port = process.env.PORT || 5000
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, world!')
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000')
 })
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
